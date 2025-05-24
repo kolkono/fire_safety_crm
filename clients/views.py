ae89725fa5808f26e8b14e04ebe6afab8a4069df
kolkono <kolkono2@yandex.ru>
2025-05-24 10:10:16 +0300

    Добавлен отчёт по клиентам с графиком Plotly, исправлено отображение города в клиентских данных

--- clients/views.py
+++ clients/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import ClientForm
-from rest_framework import generics
 from .models import Client
+from .forms import ClientForm
 from .serializers import ClientSerializer
+from django.db.models import Count
+import plotly.express as px
+from rest_framework import generics
 
+# HTML Views
 def client_list(request):
     clients = Client.objects.all()
     return render(request, 'clients/client_list.html', {'clients': clients})
@@ -29,6 +32,22 @@ def client_edit(request, pk):
         form = ClientForm(instance=client)
     return render(request, 'clients/client_form.html', {'form': form, 'title': 'Редактировать клиента'})
 
+# Plotly Report View (по городам)
+def client_report(request):
+    data = Client.objects.values('city').annotate(count=Count('id')).order_by('city')
+    cities = [entry['city'] for entry in data]
+    counts = [entry['count'] for entry in data]
+
+    fig = px.bar(
+        x=cities,
+        y=counts,
+        labels={'x': 'Город', 'y': 'Количество клиентов'},
+        title='Клиенты по городам'
+    )
+    chart = fig.to_html(full_html=False)
+    return render(request, 'clients/client_report.html', {'chart': chart})
+
+# API Views
 class ClientListCreateAPIView(generics.ListCreateAPIView):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
