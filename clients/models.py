da4a53a9c46fce1bec128d2b1b9335b4d5f9dbc1
kolkono <kolkono2@yandex.ru>
2025-05-24 08:33:18 +0300

    Добавлено приложение clients с моделью Client и валидацией

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import RegexValidator, EmailValidator
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(
+        max_length=20,
+        validators=[RegexValidator(regex=r'^\+?\d{9,15}$', message="Введите корректный номер телефона")]
+    )
+    email = models.EmailField(validators=[EmailValidator(message="Введите корректный email")])
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
