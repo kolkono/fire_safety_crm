0dde0e656a0154d5bf7e078e85e64d0fff958746
kolkono <kolkono2@yandex.ru>
2025-05-24 08:43:53 +0300

    Добавлена модель Client, миграции и форма с валидацией

--- fire_safety_crm/urls.py
+++ fire_safety_crm/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('clients/', include('clients.urls')),
+    path('clients/', include('clients.urls')),  # подключаем routes из clients
     path('admin/', admin.site.urls),
 ]
